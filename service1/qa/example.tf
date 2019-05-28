c997f141219e349eb877d277fca39c525079a443
masa3 <masa@hashicorp.com>
2019-05-28 11:27:28 +0900

    modified structure

---
+++ service1/qa/example.tf
@@ -0,0 +1,14 @@
+variable "file_count" {
+  default = 5
+}
+
+resource "local_file" "foo" {
+  count    = var.file_count
+  content  = "foo! by Masatomo Ito. This is file ${count.index}"
+  filename = "${path.module}/foo_${count.index}.bar"
+}
+
+output "filename" {
+  value = local_file.foo.*.filename
+}
+
---
+++ service1/qa/example2.tf
@@ -0,0 +1,14 @@
+variable "file_count2" {
+  default = 10
+}
+
+resource "local_file" "foo2" {
+  count    = var.file_count
+  content  = "foo! by Masatomo Ito. This is file ${count.index}"
+  filename = "${path.module}/foo2_${count.index}.bar"
+}
+
+output "filename2" {
+  value = local_file.foo.*.filename
+}
+
