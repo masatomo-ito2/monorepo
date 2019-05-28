bb8e36077adef1991532e60e2da015870b6d0c38
masa3 <masa@hashicorp.com>
2019-05-28 11:14:05 +0900

    fix

--- service1/example2.tf
+++ service1/example2.tf
@@ -1,14 +1,14 @@
-variable "file_count" {
+variable "file_count2" {
   default = 5
 }
 
-resource "local_file" "foo" {
+resource "local_file" "foo2" {
   count    = var.file_count
   content  = "foo! by Masatomo Ito. This is file ${count.index}"
   filename = "${path.module}/foo2_${count.index}.bar"
 }
 
-output "filename" {
+output "filename2" {
   value = local_file.foo.*.filename
 }
 
